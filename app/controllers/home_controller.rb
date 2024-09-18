cb52a29e0cf63f6f946ceda4fec5ce260b33572a
잠자는사자 <admin@sleepinglion.pe.kr>
2024-09-19 01:25:33 +0900

    수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,19 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, except: [ :index]  
-  
-  def index  
-    
+  before_action :authenticate_user!, except: [ :index]
+
+  def index
+
+
+  end
+
+  def initialize(*params)
+    super(*params)
+    @title=t('default_title')
+    @page_itemtype="http://schema.org/WebPage"
     @script='home/index'
   end
-  
+
   def no_auth
-  
+
   end
 end
