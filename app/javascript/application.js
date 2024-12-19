2ec31693d8dd5c469eeb5ed02766162e45d80578
sleepinglion <admin@sleepinglion.pe.kr>
2024-12-19 23:42:45 +0900

    modified:   .idea/company.iml 	modified:   .idea/workspace.xml 	modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/javascript/application.js 	modified:   app/javascript/index.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-
 import Rails from '@rails/ujs';
 window.Rails = Rails;
 
@@ -9,8 +7,6 @@ import * as bootstrap from 'bootstrap';
 window.Modal = bootstrap.Modal;
 
 import './add_jquery'
-import 'jquery-ui-dist/jquery-ui.min'
-import './jquery.pagination';
 import './common'
 import './index'
 
--- app/javascript/index.js
+++ app/javascript/index.js
@@ -3,6 +3,9 @@ var ready=function(){
 };
 
 $(document).ready(ready);
+
+document.addEventListener("turbo:load", ready);
+
 document.addEventListener("DOMContentLoaded", (event) => {
     Rails.start();
 });
