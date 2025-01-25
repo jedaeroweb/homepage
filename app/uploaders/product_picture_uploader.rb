82149b45dc40f52874fd597ee9827a5146d75c7c
sleepinglion(desktop) <admin@sleepinglion.pe.kr>
2025-01-25 12:40:16 +0900

    수정

--- app/uploaders/product_picture_uploader.rb
+++ app/uploaders/product_picture_uploader.rb
@@ -1,7 +1,7 @@
 class ProductPictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
+  #include CarrierWave::MiniMagick
 
   storage :file
 
