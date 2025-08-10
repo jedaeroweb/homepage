3823bad9c91689b505020c02c83d3bb8dfc3d4c2
sleepinglion <admin@jedaeroweb.co.kr>
2025-08-11 02:01:32 +0900

    수정함:        .gitignore 	수정함:        app/assets/stylesheets/front/application.scss 	수정함:        app/javascript/application.js 	수정함:        app/views/layouts/_footer.html.erb 	수정함:        config/deploy.rb 	수정함:        config/locales/en.yml 	수정함:        config/locales/ko.yml 	수정함:        config/locales/zh-CN.yml

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "rails-ujs"
+import jQuery from "jquery";
+// jQuery를 전역 변수로 설정
+window.$ = window.jQuery = jQuery;
 
 //import "trix"
 import * as bootstrap from 'bootstrap';
