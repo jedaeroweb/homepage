6bdaf3cc40c8644e5d7617c8da5a81a5e7738671
sleepinglion <admin@jedaeroweb.co.kr>
2026-01-02 13:47:34 +0900

    수정함:        app/models/order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,12 @@ class Order < ApplicationRecord
   after_initialize :default_values
 
   validates_presence_of :user_id,:price,:discount,:payment
-  validates_presence_of :title
+  validates :title,
+            presence: true,
+            length: {
+              maximum: 60,
+              too_long: "제목은 60자 이내여야 합니다."
+            }
   belongs_to :user, counter_cache: true
   has_many :accounts_orders, dependent: :destroy
   has_many :orders_products, dependent: :destroy
