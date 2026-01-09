ec7892f5e2fc6a6acb5faec4c72920be803acaf2
sleepinglion <admin@jedaeroweb.co.kr>
2026-01-09 13:04:41 +0900

    수정

--- config/deploy.sample.rb
+++ config/deploy.sample.rb
@@ -1,15 +1,10 @@
-# config valid for current version and patch releases of Capistrano
-#lock "~> 3.15.0"
 
-set :application, "company"
+set :application, "your_name"
 set :repo_url, "git@github.com:jedaeroweb/homepage.git"
 set :branch, "main"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/company"
+set :deploy_to, "your_path"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
